66069d17c5e8a586498fe5b2b644cc89cc2aba9b
Johnnycakeshit <jonathan_mandl@yahoo.com>
2015-03-17 17:37:33 -0400

    Update compose.php

--- compose.php
+++ compose.php
@@ -3,9 +3,17 @@
 
 </head>
 <body>
+<form>
+To: <input type="text" name="Reciever"> 
+Subject <input type="text"name="subject">
+</form>
   <textarea rows="20" cols="50">
   This is where the mail goes, change this later
   </textarea>
   
 </body>
 </html>
+<?php
+
+
+?>
